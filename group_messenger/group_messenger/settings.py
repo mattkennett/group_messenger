33aefdbc2a6445ce06808c18ac3023ce23b784c0
Matt Kennett <matt@mattkennett.com>
2019-01-06 21:31:18 -0600

    Remove Session Authentication

--- group_messenger/group_messenger/settings.py
+++ group_messenger/group_messenger/settings.py
@@ -137,6 +137,5 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     )
 }
